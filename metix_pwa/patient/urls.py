2418c9318b04fe25067a1db5831fd096580a6c1c
Ivo <2310555d@student.gla.ac.uk>
2019-10-19 17:35:09 +0100

    medicine page done

--- metix_pwa/patient/urls.py
+++ metix_pwa/patient/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     url(r'^$', views.patient_index, name='patient_index'),
     url(r'^take_pill/$', views.take_pill, name='take_pill'),
     url(r'^put_pill_back/$', views.put_pill_back, name='put_pill_back'),
+    url(r'^test/$', views.test, name='test'),
 
 ]
